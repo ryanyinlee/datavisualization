1b17b9a92ef184a6295ada671dd3c41e4a12e6f1
Ryan Lee <ryanyinlee@protonmail.com>
2023-03-20 23:46:39 -0700

    learning to graph

---
+++ scatter_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+xvals = range(1,1001)
+
+yvals = [x**2 for x in xvals]
+
+plt.style.use('seaborn')
+
+fig, ax = plt.subplots()
+ax.scatter(xvals,yvals, c='red',s=10)
+
+ax.set_title('square nums', fontsize = 24)
+ax.set_xlabel('value', fontsize = 14)
+ax.set_ylabel('square of value', fontsize = 14)
+
+ax.tick_params(axis = 'both', which='major', labelsize = 14)
+
+ax.axis([0, 1100, 0, 1_100_000])
+
+plt.show()
