35bce11e5f3980147baf150d4b25848f20b11503
Ryan Lee <ryanyinlee@protonmail.com>
2023-03-23 10:00:03 -0700

    just two dice

--- dicevisual.py
+++ dicevisual.py
@@ -3,25 +3,28 @@ from plotly import offline
 
 from dice import Die
 
-die = Die()
+die0 = Die()
+die1 = Die()
 
 results = []
 
 for rollnum in range(1000):
-    result = die.roll()
+    result = die0.roll() + die1.roll()
     results.append(result)
 
 frequencies = []
 
-for value in range(1, die.numsides + 1):
+maxresult = die0.numsides + die1.numsides
+
+for value in range(2, maxresult + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-xvals = list(range(1, die.numsides+1))
+xvals = list(range(2, maxresult + 1))
 data = [Bar(x=xvals, y=frequencies)]
 
-x_axis_config = {'title': 'Result'}
+x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
 
-my_layout = Layout(title='Results of rolling a D6 1000 times', xaxis = x_axis_config, yaxis = y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
+my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis = x_axis_config, yaxis = y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='2xd6.html')
