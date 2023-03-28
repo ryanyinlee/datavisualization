698f9a24341077882ee5799269a5a4d048fb8cc0
Ryan Lee <ryanyinlee@protonmail.com>
2023-03-28 12:35:06 -0700

    working on TIYs, added rainfall comparison

---
+++ sitka_rainfall.py
@@ -0,0 +1,38 @@
+import csv
+
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+
+filename = 'data/sitka_weather_2018_simple.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    headerrow = next(reader)
+    
+    rainfall = []
+    dates = []
+
+    for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
+        try:
+            rain = float(row[3])
+        except:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            rainfall.append(rain)
+
+
+plt.style.use('seaborn')
+
+fig, ax = plt.subplots()
+ax.plot(dates, rainfall, c='green')
+
+ax.set_title('Sitka Rainfall, 2018', fontsize = 24)
+ax.set_xlabel('', fontsize = 16)
+fig.autofmt_xdate()
+ax.set_ylabel('Rainfall Per Day (Inches)', fontsize = 16)
+ax.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
