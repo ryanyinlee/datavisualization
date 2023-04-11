cd0dbfe90526f6c4c9c36eccfe9df92bc86dba31
Ryan Lee <ryanyinlee@protonmail.com>
2023-04-10 22:51:42 -0700

    Working with APIs. Using Git repo API.

---
+++ datavisapi/githubrepoapicall.py
@@ -0,0 +1,27 @@
+import requests
+
+from plotly.graph_objs import Bar
+from plotly import offline
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+response_dict = r.json()
+print(f"Total Repos: {response_dict['total_count']}")
+
+repo_dicts = response_dict['items']
+repo_names = []
+stars = []
+print(f"Repos Returned: {len(repo_dicts)}")
+
+for repo_dict in repo_dicts:
+    print(f"\nName: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repo URL: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+
